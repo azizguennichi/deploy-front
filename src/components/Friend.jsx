0e1c1be9ff12e5416ebb3030fd973c7d18d547a5
azizguennichi <ezzoguennichi@gmail.com>
2023-01-31 19:18:36 +0100

    second commit

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://technotes-api.onrender.com/users/${_id}/${friendId}`,
+      `https://aziz-api.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
